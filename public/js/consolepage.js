cce008c9384efb733c09b4325e4997345016dfe9
kellynash <kellynash@gmail.com>
2015-11-07 13:17:42 -0700

    Edited console greeting to include storyline and invitation to join. Edited format of rules to display properly on console

--- public/js/consolepage.js
+++ public/js/consolepage.js
@@ -17,9 +17,9 @@ var sendToServer = function(term) {
 
 
         if(data != null){
-            term.echo('Success! Welcome home, Agent.');
+            term.echo('Success! Welcome back, Agent.');
         } else {
-            term.echo(email + "Didn't work!");
+            term.echo(email + " didn't work!");
         }
     }).fail(function(res){
         term.echo(command + "Sorry. Our super-secret servers are currently down.");
@@ -35,9 +35,9 @@ var loginServer = function(term) {
     .done(function (data){
 
         if(data != null){
-            term.echo('Success! Welcome home, Agent.');
+            term.echo('Success! Welcome back, Agent.');
         } else {
-            term.echo(email + "Didn't work!");
+            term.echo(email + " didn't work!");
         }
     }).fail(function(res){
         term.echo(command + "Sorry. Our super-secret servers are currently down.");
@@ -56,22 +56,21 @@ var profile = function(term) {
     });
 }
 var rules = function(term) {
-    term.echo('                                                                                                    ' + 
-        'Goal: Be the last agent standing.                                                                                                                                                                       ' +
-        'How to play: Each agent will be assigned one target to eliminate, and each agent will in turn be    ' + 
-        'pursued by another player. As no one knows who else may be playing, secrecy and stealth are         '+ 
-        'paramount. (To maintain a low profile, be discreet when eliminating your target, and think twice    '+ 
-        'before casually discussing the game with others.)  All games begin and end at midnight.                                                                                                                 ' +  
-        'Eliminations:  When you eliminate an agent they will surrender their badge number to you. Enter     ' +
-        'this number on your profile page to confirm the deed and receive your new target.  Follow the       ' + 
-        'same proceedure if you are eliminated. (Note: Your badge number is assigned when you sign up.       ' + 
-        'It can be found on your profile page.)                                                                                                                                                                  ' +
-        'Rules: No weapons of any kind are allowed. Players are eliminated by touching with fingers.         ' + 
-        'To maintain your cover, all eliminations must be covert. You may not enter a dorm room uninvited.   ' + 
-        'All areas of campus are fair game except the following: classrooms, the library, and a              ' +
-        'player’s place of employment.                                                                                                                                                                           ' +
-        'Keep in mind this is a game that rewards sneakiness and vigilance. Keep your identity concealed.    ' + 
-        'The winner gets full bragging rights within the Paranoia community. Play fair; don’t cheat.         ' +
+    term.echo('\nGoal: Be the last agent standing.\n' + 
+        'How to play: Each agent will be assigned one target to eliminate, and each agent will in turn be\n'+ 
+        'pursued by another player. As no one knows who else may be playing, secrecy and stealth are\n' + 
+        'paramount. (To maintain a low profile, be discreet when eliminating your target, and think twice \n' + 
+        'before casually discussing the game with others.)  All games begin and end at midnight.\n\n' +  
+        'Eliminations:  When you eliminate an agent they will surrender their badge number to you. Enter\n' +
+        'this number on your profile page to confirm the deed and receive your new target.  Follow the\n' + 
+        'same proceedure if you are eliminated. (Note: Your badge number is assigned when you sign up.\n' + 
+        'It can be found on your profile page.)\n\n' +
+        'Rules: No weapons of any kind are allowed. Players are eliminated by touching with fingers.\n' + 
+        'To maintain your cover, all eliminations must be covert. You may not enter a dorm room uninvited.\n' + 
+        'All areas of campus are fair game except the following: classrooms, the library, and a\n' +
+        'player’s place of employment.\n\n' +
+        'Keep in mind this is a game that rewards sneakiness and vigilance. Keep your identity concealed.\n' + 
+        'The winner gets full bragging rights within the Paranoia community. Play fair; don’t cheat.\n' +
         'Above all, HAVE FUN!!');
 }
 
@@ -121,7 +120,7 @@ jQuery(document).ready(function($) {
             term.echo('');
             term.echo('');
             term.echo('');
-            term.echo('Type \'profile\' to view your personal page.');
+            term.echo('Type \'profile\' to view your dossier.');
 
         } else if (inLogin && savedHandle === null) {
             loginHandleCheck(); 
@@ -145,10 +144,16 @@ jQuery(document).ready(function($) {
         } else if (command == 'letmein') {
             login(term);
         } else {
-            term.echo("unknown command " + command);
+            term.echo("Unknown command " + command);
         }
     }, {
-        greetings: "To create an account, type 'signup'. ",
+        greetings: "Greetings. Would you like to play a game?\n" + 
+        "\nA word of warning before you agree: know that this is no ordinary game.  It is not for the\n" + 
+        "faint of heart.  Paranoia rewards stealth, secrecy and cunning.  Upon admission, you will enter\n" + 
+        "a clandestine world of secret agents bent on your destruction. However, as you are pursued, \n" +
+        "so shall you pursue.  Your goal is to be the last agent standing.  The thrill of the chase is\n" + 
+        "its own reward." + 
+        "\n\nTo accept the challenge and join our secret society, type 'signup'. ",
         onBlur: function() {
             // prevent loosing focus
             return false;
